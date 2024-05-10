9e5a7a9a93b444a6f54e21a100da9c223d952a48
doswald32 <dan.oswald32@gmail.com>
2024-05-09 20:28:47 -0600

    created Team component

--- phase-2-final-project/src/NavBar.js
+++ phase-2-final-project/src/NavBar.js
@@ -1,7 +1,27 @@
 function NavBar() {
     return (
-
-    )
+        <nav>
+            <NavLink
+            to="/"
+            className="nav-link"
+            >
+              Home
+            </NavLink>
+            <NavLink
+            to="/about"
+            className="nav-link"
+            >
+              About
+            </NavLink>
+            <NavLink 
+            to="/login"
+            className="nav-link"
+            >
+              Login
+            </NavLink>
+        </nav>
+    );
+};
 }
 
 export default NavBar
