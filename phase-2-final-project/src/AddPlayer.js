dcc56ab49fa916fdc5a84949162b80b2816f84ab
doswald32 <dan.oswald32@gmail.com>
2024-05-09 21:30:59 -0600

    added inputs to addplayer form

--- phase-2-final-project/src/AddPlayer.js
+++ phase-2-final-project/src/AddPlayer.js
@@ -7,7 +7,28 @@ function AddPlayer() {
                 <NavBar />
             </header>
             <main>
-                
+                <form>
+                    <label>First Name: </label>
+                    <input type="text"/>
+                    <label>Last Name: </label>
+                    <input type="text"/>
+                    <label>Position: </label>
+                    <input type="text"/>
+                    <label>PPPG: </label>
+                    <input type="text"/>
+                    <label>APG: </label>
+                    <input type="text"/>
+                    <label>RPG: </label>
+                    <input type="text"/>
+                    <label>Age: </label>
+                    <input type="text"/>
+                    <label>Salary: </label>
+                    <input type="text"/>
+                    <label>Image URL: </label>
+                    <input type="text"/>
+                    <br/>
+                    <button>Submit</button>
+                </form>
             </main>
         </>
     )
